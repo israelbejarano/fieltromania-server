b009c478c9ea68e0fb115dadefb4b407da2c352f
israel <israel.bejarano.a@gmail.com>
2019-09-28 11:51:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Backend-Server para la app de FieltroManía
+
+Este es el código necesario para arrancar el backend conectado a MongoDB usando Mongoose. Para ejecutarlo, es necesario instalar los módulos de node usando el comando:
+
+```
+npm install
+```
+
+## Documentación de la API
---
+++ app.js
@@ -0,0 +1,35 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// Inicializar variables
+var app = express();
+
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    next();
+});
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+
+// Importar rutas
+
+// Conexion a BBDD
+mongoose.connection.openUri('mongodb://localhost:27017/fieltroManiaDB', (err, res) => {
+    if (err) throw err;
+    console.log('BBDD en puerto 27017: \x1b[32m%s\x1b[0m', 'online');
+});
+
+
+// Rutas
+
+// Escuchas
+app.listen(3000, () => {
+    console.log('Node/Express en puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
