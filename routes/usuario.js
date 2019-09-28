4de247469ef9c02f630ca7e665a87f4872f77bad
israel <israel.bejarano.a@gmail.com>
2019-09-28 14:19:33 +0200

    get usuario paginados + token security

--- routes/usuario.js
+++ routes/usuario.js
@@ -7,16 +7,15 @@ var app = express();
 var Usuario = require('../models/usuario');
 
 // ========================================
-// obtener todos los usuarios. El listado
-// siempre se muestra aun no teniendo token
+// obtener todos los usuarios sin paginacion
 // ========================================
-app.get('/', (req, res, next) => {
+app.get('/todos', [mdAutenticacion.verificarToken], (req, res, next) => {
 
     var desde = req.query.desde || 0;
     desde = Number(desde);
 
     Usuario.find({}, (err, usuarios) => { // devuelve todo el objeto usuario sin mas
-        /* Usuario.find({}, 'nombre apellidos email img role google')
+        /* Usuario.find({}, 'nombre apellidos email avatar role google')
             .skip(desde) // para ir paginando se salta los desde usuarios
             .limit(5) // paginacion de 5 elementos por paginas
             .exec((err, usuarios) => { */
@@ -45,7 +44,74 @@ app.get('/', (req, res, next) => {
 });
 
 // ========================================
-// crear un nuevo usuario
+// obtener todos los usuarios con paginacion
+// ========================================
+app.get('/', [mdAutenticacion.verificarToken], (req, res, next) => {
+
+    var desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    Usuario.find({}, 'nombre apellidos email avatar role google')
+        .skip(desde) // para ir paginando se salta los desde usuarios
+        .limit(5) // paginacion de 5 elementos por paginas
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error cargando usuario de BBDD',
+                    errors: err
+                });
+            }
+            Usuario.count({}, (err, total) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error contando usuarios de BBDD',
+                        errors: err
+                    });
+                }
+                res.status(200).json({
+                    ok: true,
+                    usuarios: usuarios, //segun EM6 esto es redundante y se puede hacer solo usuarios, pero asi queda mas claro
+                    total: total
+                });
+            });
+        });
+});
+
+// ========================================
+// Obtener usuario por id
+// ========================================
+app.get('/:id', [mdAutenticacion.verificarToken], (req, res) => {
+    var id = req.params.id;
+    Usuario.findById(id, 'nombre apellidos email avatar role google', (err, usuario) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error cargando usuario de BBDD',
+                errors: err
+            });
+        }
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con id ' + id + ' no existe',
+                errors: { message: 'No existe usuario con este ID' }
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            usuario
+        });
+    });
+});
+
+// ========================================
+// crear un nuevo usuario. No necesito token
+// porque será el registro de una nueva cuenta
+// el encargado de eso. Este método es para
+// crear usuario rapidamente desde el postman
+// TODO en un futuro habra una ruta register.
 // ========================================
 app.post('/', (req, res) => {
     var body = req.body;
