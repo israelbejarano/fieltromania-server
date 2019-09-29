b00dfdf3a4450fc57ac7231c668783883b9e336d
israel <israel.bejarano.a@gmail.com>
2019-09-29 10:41:44 +0200

    obtener productos con paginacion

--- routes/producto.js
+++ routes/producto.js
@@ -35,4 +35,39 @@ app.get('/todos', [mdAutenticacion.verificarToken], (req, res) => {
     });
 });
 
+// ========================================
+// obtener todos los productos con paginacion
+// ========================================
+app.get('/', [mdAutenticacion.verificarToken], (req, res) => {
+
+    var desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    Producto.find({})
+        .skip(desde) // para ir paginando se salta los desde usuarios
+        .limit(5) // paginacion de 5 elementos por paginas
+        .exec((err, productos) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error cargando producto de BBDD',
+                    errors: err
+                });
+            }
+            Producto.count({}, (err, total) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error contando productos de BBDD',
+                        errors: err
+                    });
+                }
+                res.status(200).json({
+                    ok: true,
+                    productos,
+                    total
+                });
+            });
+        });
+});
 module.exports = app; //con esto puedo usar este fichero en cualquier parte del server
